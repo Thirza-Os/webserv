a9d3e2c1c06bc2804c88864d83c9cc73722c8192
Jan Zaremba <jzaremba@f1r5s4.codam.nl>
2023-10-23 17:06:49 +0200

    Add more robust uri processing

--- WebServer/ResponseBuilder.cpp
+++ WebServer/ResponseBuilder.cpp
@@ -4,12 +4,12 @@
 #include <fstream>
 #include <sstream>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <cstring>
 
 ResponseBuilder::ResponseBuilder(RequestParser request, ServerConfig config): _request(request), _config(config) {
     std::cout << "Building response.." << std::endl;
     this->_status_code = this->_request.get_status_code();
-    std::cout << "checking status code.." << std::endl;
     if (this->_request.get_method() == "GET"){
         build_response();
     }
@@ -38,11 +38,11 @@ ResponseBuilder &ResponseBuilder::operator=(const ResponseBuilder &src)
 Location    ResponseBuilder::match_location(std::string uri) {
     Location empty{};
     std::vector<Location> locations = this->_config.get_locations();
-    std::cout << "Locations retrieved.." << std::endl;
     for (std::vector<Location>::iterator it = locations.begin(); it != locations.end(); it++) {
         if (it->path != "/") {
             if (uri.find(it->path, 0) != std::string::npos) {
                 if (it->returnPath.empty()) {
+                    std::cout << "Location matched" << std::endl;
                     return (*it);
                 }
                 else {
@@ -51,22 +51,20 @@ Location    ResponseBuilder::match_location(std::string uri) {
             }
         }
     }
-    std::cout << "No match, matching just '/'.." << std::endl;
-    //loop again and find a potential match for just a '/'
+    //loop again and find a potential match for just a '/' which should always match if it exists
     for (std::vector<Location>::iterator it = locations.begin(); it != locations.end(); it++) {
         if (it->path == "/") {
-            if (uri.find(it->path, 0) != std::string::npos) {
-                if (it->returnPath.empty()) {
-                    return (*it);
-                }
-                else {
-                    return (match_location(it->returnPath));
-                }
+            if (it->returnPath.empty()) {
+                std::cout << "Location matched, just /" << std::endl;
+                return (*it);
+            }
+            else {
+                return (match_location(it->returnPath));
             }
         }
     }
-    std::cout << "No location match.." << std::endl;
-    //if there are no locations, return an empty location struct
+    std::cout << "No location match" << std::endl;
+    //if there are no locations or no matches, return an empty location struct
     return (empty);
 }
 
@@ -101,63 +99,65 @@ std::string ResponseBuilder::process_uri() {
     std::cout << "Processing uri.." << std::endl;
     std::string uri = this->_request.get_uri();
     Location matched_loc = match_location(uri);
-    std::cout << "Location matched.." << std::endl;
     std::string dflt_index;
     if (!this->_config.get_index().empty()) {
         dflt_index = this->_config.get_index().front();
     }
-    std::cout << "Still ok for now?.." << std::endl;
     if (dflt_index.empty()) {
         dflt_index = "index.html";
     }
-    std::cout << "Erasing possible leading /.." << std::endl;
-    if (uri.front() == '/') {
-        uri.erase(0, 1);
-    }
-    std::cout << "requested uri without a / at the start: " << uri << std::endl;
     if (!matched_loc.path.empty()) {
         if (matched_loc.methods[0]) { //check if GET method is even allowed or not
-        std::cout << "matched a location " << matched_loc.path << "!!!" << std::endl;
+        std::cout << "matched a location " << matched_loc.path << "!" << std::endl;
         std::cout << "index: " << matched_loc.index << std::endl;
+        std::cout << "requested uri: " << uri << std::endl;
             if (uri.find(matched_loc.path, 0) == std::string::npos) {
                 //if uri and path don't match, this was a redirect
                 //and we should change the uri accordingly
                 //erase until a slash then add path instead
+                //if there are no slashes, replace whole uri with path
+                if (uri.front() == '/') {
+                    uri.erase(0, 1);
+                }
                 if (uri.find_first_of('/') != std::string::npos) {
                     uri.erase(0, uri.find_first_of('/'));
                     uri.insert(0, matched_loc.path);
-                    if (uri.front() == '/') {
-                        uri.erase(0, 1);
-                    }
+                }
+                else {
+                    uri = matched_loc.path;
                 }
             }
-            std::cout << "what does my uri look like now?: " << uri << std::endl;
-            uri.insert(0, "/");
             if (matched_loc.root.empty()) {
                 uri.insert(0, this->_config.get_rootdirectory());
             }
             uri.insert(0, matched_loc.root);
-            if (uri.back() == '/') {
-                uri.append(matched_loc.index);
+            if (!matched_loc.index.empty()) {
+                dflt_index = matched_loc.index;
             }
             if (uri.back() == '/') {
                 uri.append(dflt_index);
             }
-            std::cout << "attempting to send this uri: " << uri << std::endl;
         }
         else {
             this->_status_code = 405;
         }
     }
     else {
-        std::cout << "empty location?.." << std::endl;
         uri.insert(0, "/");
         uri.insert(0, this->_config.get_rootdirectory());
         if (uri.back() == '/') {
             uri.append(dflt_index);
         }
-        std::cout << "attempting to send this uri: " << uri << std::endl;
     }
+    struct stat s;
+    if (lstat(uri.c_str(), &s) == 0) {
+        if (S_ISDIR(s.st_mode)) {
+            std::cout << "is a directory" << std::endl;
+            uri.append("/");
+            uri.append(dflt_index);
+        }
+    }
+    std::cout << "attempting to send this uri: " << uri << std::endl;
     return (uri);
 }
 
@@ -168,7 +168,6 @@ void	ResponseBuilder::build_response() {
         this->_response = this->_header;
         return ;
     }
-    std::cout << "Request is good it seems.." << std::endl;
     std::string uri = process_uri();
     std::ifstream htmlFile(uri.c_str());
     if (this->_status_code == 405) {
