78835467c9b7fa35a84fc2fd356702b7ad42c1a8
Jan Zaremba <jzaremba@f1r6s13.codam.nl>
2023-10-25 17:28:29 +0200

    Fix many issues with reading and sending cgi response

--- WebServer/www/penguinserv/cgiRoute/coolCgi.php
+++ WebServer/www/penguinserv/cgiRoute/coolCgi.php
@@ -1,4 +1,6 @@
 #!/usr/bin/php
 <?php
-echo "Hello, this is a cool cgi!";
+echo "HTTP/1.1 200 OK\n";
+echo "Content-type: text/html\n\n";
+echo "<HTML><TITLE>My cool cgi!</TITLE><BODY><H1>Hello, this is a cool cgi!</H1></BODY></HTML>";
 ?>
